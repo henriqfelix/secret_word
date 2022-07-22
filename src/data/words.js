1ff76c4ab4d61deb1ba9c8fb01cfba44f0800a89
henriqfelix <henriquefelix1821@gmail.com>
2022-07-22 08:58:18 -0300

    limpar arquivos bases e criar pasta data

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordslist = {
+  carro: ["motor", "capô", "banco", "volante", "câmbio", "roda", "freio"],
+  fruta: ["maçã", "pera", "goiaba", "banana", "uva", "abacate", "melancia"],
+  corpo: ["braço", "perna", "olho", "umbigo", "dente", "gengiva", "unha"],
+  computador: ["mouse", "teclado", "monitor", "gabinete", "processador"],
+  programação: ["java", "javascript", "ruby", "tailwind", "flutter"],
+  alimento: ["arroz", "carne", "feijão", "leite", "ovo", "linguiça"],
+};
