b3df23c096cdbc5073e56a9ab098018429f346f5
henriqfelix <henriquefelix1821@gmail.com>
2022-07-22 13:54:57 -0300

    implementar css do game

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,9 +2,30 @@ import "./Game.css";
 
 const Game = ({ verifyLetter }) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar jogo</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 0</span>
+      </p>
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica: <span>Dica ...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="BlankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras incorretas:</p>
+        <span>a, </span>
+        <span>b, </span>
+      </div>
     </div>
   );
 };
